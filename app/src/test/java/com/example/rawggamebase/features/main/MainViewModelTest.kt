783b14dbaf5ccd7e52c752419f6685ba37d125c5
ramadhancm <ramadhan.maulana@ist.id>
2023-08-25 10:37:10 +0700

    add unit test for MainViewModel

---
+++ app/src/test/java/com/example/rawggamebase/features/main/MainViewModelTest.kt
@@ -0,0 +1,95 @@
+package com.example.rawggamebase.features.main
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.rawggamebase.data.GameRepository
+import com.example.rawggamebase.data.dto.Game
+import com.example.rawggamebase.data.dto.Result
+import com.example.rawggamebase.features.adapters.GameListAdapter
+import com.example.rawggamebase.features.list.ListViewModel
+import com.example.rawggamebase.features.model.GameModel
+import com.example.rawggamebase.utils.UiState
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainViewModelTest {
+
+    private lateinit var viewModel: MainViewModel
+    private val gameRepo: GameRepository = mockk(relaxed = true)
+    private val dispatcher: TestDispatcher = UnconfinedTestDispatcher()
+    private lateinit var uiState: MutableList<UiState<List<GameModel>>>
+
+    @get:Rule
+    val rule: TestRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        viewModel = MainViewModel(gameRepo, dispatcher)
+        uiState = mutableListOf()
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun `init and returning data`() = runTest {
+        val returnedList = listOf(
+            Game(
+                id = 1,
+                name = "Hello",
+                backgroundImage = "backgroundImage",
+                rating = 4.5,
+                released = "14-14-2020"
+            )
+        )
+        val uiData = listOf(
+            GameModel(
+                id = 1,
+                title = "Hello",
+                imageLink = "backgroundImage",
+                rating = "4.5",
+                releaseDate = "14-14-2020"
+            ),
+            GameModel(
+                viewType = GameListAdapter.VIEW_MORE
+            )
+        )
+        coEvery {
+            gameRepo.getGames(searchKey = any(), page = any())
+        } returns Result.Success(data = returnedList)
+
+        val job = launch(dispatcher) {
+            viewModel
+                .gameList
+                .take(3)
+                .toList(uiState)
+        }
+
+        advanceUntilIdle()
+        job.cancel()
+
+        coVerify {
+            gameRepo.getGames(null, null)
+        }
+        assertEquals(UiState.Success(uiData), uiState.last())
+    }
+}
