5b19b7eb995509ff69d3855b24963531f40a4112
ramadhancm <ramadhan.maulana@ist.id>
2023-12-26 15:51:20 +0700

    remove data unit test in Suite

--- app/src/test/java/com/example/rawggamebase/SuiteTest.kt
+++ app/src/test/java/com/example/rawggamebase/SuiteTest.kt
@@ -1,6 +1,5 @@
 package com.example.rawggamebase
 
-import com.example.data.data.GameRepositoryTest
 import com.example.rawggamebase.features.detail.GameDetailViewModelTest
 import com.example.rawggamebase.features.list.ListViewModelTest
 import com.example.rawggamebase.features.main.MainViewModelTest
@@ -12,6 +11,5 @@ import org.junit.runners.Suite
     MainViewModelTest::class,
     GameDetailViewModelTest::class,
     ListViewModelTest::class,
-    com.example.data.data.GameRepositoryTest::class
 )
 class SuiteTest
